33ba8a40907b11f4459f1880f780040d600eb28d
wilmergvara <wilmer.guevara01@epn.edu.ec>
2022-02-11 13:42:08 -0500

    Nombre

--- Servicios Aeroportuarios/DATOS.Designer.cs
+++ Servicios Aeroportuarios/DATOS.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.btnSiguiente = new System.Windows.Forms.Button();
+            this.lblDatos = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnSiguiente
@@ -42,22 +44,47 @@
             this.btnSiguiente.UseVisualStyleBackColor = true;
             this.btnSiguiente.Click += new System.EventHandler(this.btnSiguiente_Click);
             // 
+            // lblDatos
+            // 
+            this.lblDatos.AutoSize = true;
+            this.lblDatos.Font = new System.Drawing.Font("Broadway", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDatos.Location = new System.Drawing.Point(370, 29);
+            this.lblDatos.Name = "lblDatos";
+            this.lblDatos.Size = new System.Drawing.Size(243, 68);
+            this.lblDatos.TabIndex = 1;
+            this.lblDatos.Text = "DATOS";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(51, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nombre:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(1011, 637);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblDatos);
             this.Controls.Add(this.btnSiguiente);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnSiguiente;
+        private System.Windows.Forms.Label lblDatos;
+        private System.Windows.Forms.Label label1;
     }
 }
 
