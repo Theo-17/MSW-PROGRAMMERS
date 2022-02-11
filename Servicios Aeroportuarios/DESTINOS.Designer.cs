568b2f0a6befa30410b8a49e3b3b7ccbae933d1b
Theo-17 <mateo.espinoza@epn.edu.ec>
2022-02-10 20:12:04 -0500

    Añadí labels para destinos y precios

--- Servicios Aeroportuarios/DESTINOS.Designer.cs
+++ Servicios Aeroportuarios/DESTINOS.Designer.cs
@@ -30,8 +30,13 @@
         {
             this.lblTitulo = new System.Windows.Forms.Label();
             this.btnSiguiente = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btnRegresar = new System.Windows.Forms.Button();
             this.rbtEaChile = new System.Windows.Forms.RadioButton();
+            this.rbtEaBrasil = new System.Windows.Forms.RadioButton();
+            this.rbtEaArgentina = new System.Windows.Forms.RadioButton();
+            this.lblPrecioEaC = new System.Windows.Forms.Label();
+            this.lblPrecioEaB = new System.Windows.Forms.Label();
+            this.lblPrecioEaA = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblTitulo
@@ -40,7 +45,7 @@
             this.lblTitulo.Font = new System.Drawing.Font("Broadway", 25.8F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblTitulo.Location = new System.Drawing.Point(164, 9);
             this.lblTitulo.Name = "lblTitulo";
-            this.lblTitulo.Size = new System.Drawing.Size(249, 50);
+            this.lblTitulo.Size = new System.Drawing.Size(246, 48);
             this.lblTitulo.TabIndex = 0;
             this.lblTitulo.Text = "DESTINOS";
             // 
@@ -54,22 +59,22 @@
             this.btnSiguiente.Text = "Siguiente";
             this.btnSiguiente.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnRegresar
             // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(31, 378);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(169, 43);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Regresar";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.btnRegresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegresar.Location = new System.Drawing.Point(31, 378);
+            this.btnRegresar.Name = "btnRegresar";
+            this.btnRegresar.Size = new System.Drawing.Size(169, 43);
+            this.btnRegresar.TabIndex = 2;
+            this.btnRegresar.Text = "Regresar";
+            this.btnRegresar.UseVisualStyleBackColor = true;
+            this.btnRegresar.Click += new System.EventHandler(this.button2_Click);
             // 
             // rbtEaChile
             // 
             this.rbtEaChile.AutoSize = true;
             this.rbtEaChile.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rbtEaChile.Location = new System.Drawing.Point(72, 111);
+            this.rbtEaChile.Location = new System.Drawing.Point(72, 125);
             this.rbtEaChile.Name = "rbtEaChile";
             this.rbtEaChile.Size = new System.Drawing.Size(311, 29);
             this.rbtEaChile.TabIndex = 4;
@@ -77,14 +82,73 @@
             this.rbtEaChile.Text = "Salida: Ecuador - Destino: Chile";
             this.rbtEaChile.UseVisualStyleBackColor = true;
             // 
+            // rbtEaBrasil
+            // 
+            this.rbtEaBrasil.AutoSize = true;
+            this.rbtEaBrasil.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtEaBrasil.Location = new System.Drawing.Point(72, 192);
+            this.rbtEaBrasil.Name = "rbtEaBrasil";
+            this.rbtEaBrasil.Size = new System.Drawing.Size(314, 29);
+            this.rbtEaBrasil.TabIndex = 5;
+            this.rbtEaBrasil.TabStop = true;
+            this.rbtEaBrasil.Text = "Salida: Ecuador - Destino: Brasil";
+            this.rbtEaBrasil.UseVisualStyleBackColor = true;
+            // 
+            // rbtEaArgentina
+            // 
+            this.rbtEaArgentina.AutoSize = true;
+            this.rbtEaArgentina.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtEaArgentina.Location = new System.Drawing.Point(72, 262);
+            this.rbtEaArgentina.Name = "rbtEaArgentina";
+            this.rbtEaArgentina.Size = new System.Drawing.Size(350, 29);
+            this.rbtEaArgentina.TabIndex = 6;
+            this.rbtEaArgentina.TabStop = true;
+            this.rbtEaArgentina.Text = "Salida: Ecuador - Destino: Argentina";
+            this.rbtEaArgentina.UseVisualStyleBackColor = true;
+            // 
+            // lblPrecioEaC
+            // 
+            this.lblPrecioEaC.AutoSize = true;
+            this.lblPrecioEaC.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrecioEaC.Location = new System.Drawing.Point(461, 127);
+            this.lblPrecioEaC.Name = "lblPrecioEaC";
+            this.lblPrecioEaC.Size = new System.Drawing.Size(56, 25);
+            this.lblPrecioEaC.TabIndex = 7;
+            this.lblPrecioEaC.Text = "$336";
+            // 
+            // lblPrecioEaB
+            // 
+            this.lblPrecioEaB.AutoSize = true;
+            this.lblPrecioEaB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrecioEaB.Location = new System.Drawing.Point(461, 194);
+            this.lblPrecioEaB.Name = "lblPrecioEaB";
+            this.lblPrecioEaB.Size = new System.Drawing.Size(56, 25);
+            this.lblPrecioEaB.TabIndex = 8;
+            this.lblPrecioEaB.Text = "$210";
+            // 
+            // lblPrecioEaA
+            // 
+            this.lblPrecioEaA.AutoSize = true;
+            this.lblPrecioEaA.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrecioEaA.Location = new System.Drawing.Point(461, 264);
+            this.lblPrecioEaA.Name = "lblPrecioEaA";
+            this.lblPrecioEaA.Size = new System.Drawing.Size(56, 25);
+            this.lblPrecioEaA.TabIndex = 9;
+            this.lblPrecioEaA.Text = "$460";
+            // 
             // DESTINOS
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(588, 450);
+            this.Controls.Add(this.lblPrecioEaA);
+            this.Controls.Add(this.lblPrecioEaB);
+            this.Controls.Add(this.lblPrecioEaC);
+            this.Controls.Add(this.rbtEaArgentina);
+            this.Controls.Add(this.rbtEaBrasil);
             this.Controls.Add(this.rbtEaChile);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnRegresar);
             this.Controls.Add(this.btnSiguiente);
             this.Controls.Add(this.lblTitulo);
             this.Name = "DESTINOS";
@@ -98,7 +162,12 @@
 
         private System.Windows.Forms.Label lblTitulo;
         private System.Windows.Forms.Button btnSiguiente;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnRegresar;
         private System.Windows.Forms.RadioButton rbtEaChile;
+        private System.Windows.Forms.RadioButton rbtEaBrasil;
+        private System.Windows.Forms.RadioButton rbtEaArgentina;
+        private System.Windows.Forms.Label lblPrecioEaC;
+        private System.Windows.Forms.Label lblPrecioEaB;
+        private System.Windows.Forms.Label lblPrecioEaA;
     }
 }
