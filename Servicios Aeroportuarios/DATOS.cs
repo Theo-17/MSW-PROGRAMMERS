1821764ac370dc5d52925588b3cc772b893bff5e
wilmergvara <wilmer.guevara01@epn.edu.ec>
2022-02-13 00:58:06 -0500

    Datos

--- Servicios Aeroportuarios/DATOS.cs
+++ Servicios Aeroportuarios/DATOS.cs
@@ -25,9 +25,22 @@ namespace Servicios_Aeroportuarios
             
         }
 
-        private void label2_Click(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
+
+        private void lblDatos_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
     }
 }
