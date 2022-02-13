e2840b9b3bf3969c8608a6f967e5199835adb529
Theo-17 <mateo.espinoza@epn.edu.ec>
2022-02-13 01:18:32 -0500

    Cambios

--- Servicios Aeroportuarios/DATOS.Designer.cs
+++ Servicios Aeroportuarios/DATOS.Designer.cs
@@ -342,7 +342,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(1075, 900);
+            this.ClientSize = new System.Drawing.Size(1075, 788);
             this.Controls.Add(this.textBox6);
             this.Controls.Add(this.label14);
             this.Controls.Add(this.textBox5);
