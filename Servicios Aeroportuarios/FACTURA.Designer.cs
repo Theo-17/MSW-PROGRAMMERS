31b1aa3cf31f98f73c87076f2e3b80059fdaf029
KEVIN SHANDER GONZALEZ TOMALA <kevin.gonzalez@epn.edu.ec>
2022-02-13 19:25:58 -0600

    Elaboracion de la interfaz de Factura

--- Servicios Aeroportuarios/FACTURA.Designer.cs
+++ Servicios Aeroportuarios/FACTURA.Designer.cs
@@ -29,37 +29,261 @@
         private void InitializeComponent()
         {
             this.lblTitulo = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnRegresar3 = new System.Windows.Forms.Button();
+            this.btnNuevaCompra = new System.Windows.Forms.Button();
+            this.btnFinalizar = new System.Windows.Forms.Button();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtFrom = new System.Windows.Forms.TextBox();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.lblFrom = new System.Windows.Forms.Label();
+            this.lblTo = new System.Windows.Forms.Label();
+            this.txtAerolinea = new System.Windows.Forms.TextBox();
+            this.lblAerolinea = new System.Windows.Forms.Label();
+            this.txtDate = new System.Windows.Forms.TextBox();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.txtTime = new System.Windows.Forms.TextBox();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.txtAsiento = new System.Windows.Forms.TextBox();
+            this.lblSeat = new System.Windows.Forms.Label();
+            this.txtPasaport = new System.Windows.Forms.TextBox();
+            this.txtTill = new System.Windows.Forms.TextBox();
+            this.lblPasaport = new System.Windows.Forms.Label();
+            this.lblTill = new System.Windows.Forms.Label();
+            this.rtbDatoTotal = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // lblTitulo
             // 
             this.lblTitulo.AutoSize = true;
             this.lblTitulo.Font = new System.Drawing.Font("Broadway", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitulo.Location = new System.Drawing.Point(172, 9);
+            this.lblTitulo.Location = new System.Drawing.Point(136, 9);
             this.lblTitulo.Name = "lblTitulo";
-            this.lblTitulo.Size = new System.Drawing.Size(276, 55);
+            this.lblTitulo.Size = new System.Drawing.Size(547, 55);
             this.lblTitulo.TabIndex = 0;
-            this.lblTitulo.Text = "FACTURA";
+            this.lblTitulo.Text = "PASE DE ABORDAJE";
             // 
-            // button1
+            // btnRegresar3
             // 
-            this.button1.Location = new System.Drawing.Point(97, 354);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(136, 32);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Regresar";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnRegresar3.Location = new System.Drawing.Point(12, 320);
+            this.btnRegresar3.Name = "btnRegresar3";
+            this.btnRegresar3.Size = new System.Drawing.Size(128, 32);
+            this.btnRegresar3.TabIndex = 1;
+            this.btnRegresar3.Text = "Regresar";
+            this.btnRegresar3.UseVisualStyleBackColor = true;
+            this.btnRegresar3.Click += new System.EventHandler(this.button1_Click);
             // 
-            // AEROLÍNEAS
+            // btnNuevaCompra
+            // 
+            this.btnNuevaCompra.Location = new System.Drawing.Point(146, 320);
+            this.btnNuevaCompra.Name = "btnNuevaCompra";
+            this.btnNuevaCompra.Size = new System.Drawing.Size(145, 32);
+            this.btnNuevaCompra.TabIndex = 2;
+            this.btnNuevaCompra.Text = "Iniciar Nueva Compra";
+            this.btnNuevaCompra.UseVisualStyleBackColor = true;
+            // 
+            // btnFinalizar
+            // 
+            this.btnFinalizar.Location = new System.Drawing.Point(617, 325);
+            this.btnFinalizar.Name = "btnFinalizar";
+            this.btnFinalizar.Size = new System.Drawing.Size(128, 32);
+            this.btnFinalizar.TabIndex = 3;
+            this.btnFinalizar.Text = "Finalizar";
+            this.btnFinalizar.UseVisualStyleBackColor = true;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(42, 134);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(77, 13);
+            this.lblName.TabIndex = 4;
+            this.lblName.Text = "Nombre/Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Enabled = false;
+            this.txtName.Location = new System.Drawing.Point(45, 111);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(302, 20);
+            this.txtName.TabIndex = 5;
+            // 
+            // txtFrom
+            // 
+            this.txtFrom.Enabled = false;
+            this.txtFrom.Location = new System.Drawing.Point(45, 159);
+            this.txtFrom.Name = "txtFrom";
+            this.txtFrom.Size = new System.Drawing.Size(117, 20);
+            this.txtFrom.TabIndex = 6;
+            this.txtFrom.TabStop = false;
+            // 
+            // txtTo
+            // 
+            this.txtTo.Enabled = false;
+            this.txtTo.Location = new System.Drawing.Point(45, 206);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(117, 20);
+            this.txtTo.TabIndex = 7;
+            // 
+            // lblFrom
+            // 
+            this.lblFrom.AutoSize = true;
+            this.lblFrom.Location = new System.Drawing.Point(42, 182);
+            this.lblFrom.Name = "lblFrom";
+            this.lblFrom.Size = new System.Drawing.Size(66, 13);
+            this.lblFrom.TabIndex = 8;
+            this.lblFrom.Text = "Desde/From";
+            // 
+            // lblTo
+            // 
+            this.lblTo.AutoSize = true;
+            this.lblTo.Location = new System.Drawing.Point(42, 229);
+            this.lblTo.Name = "lblTo";
+            this.lblTo.Size = new System.Drawing.Size(32, 13);
+            this.lblTo.TabIndex = 9;
+            this.lblTo.Text = "A/To";
+            // 
+            // txtAerolinea
+            // 
+            this.txtAerolinea.Enabled = false;
+            this.txtAerolinea.Location = new System.Drawing.Point(168, 159);
+            this.txtAerolinea.Name = "txtAerolinea";
+            this.txtAerolinea.Size = new System.Drawing.Size(179, 20);
+            this.txtAerolinea.TabIndex = 10;
+            // 
+            // lblAerolinea
+            // 
+            this.lblAerolinea.AutoSize = true;
+            this.lblAerolinea.Location = new System.Drawing.Point(165, 182);
+            this.lblAerolinea.Name = "lblAerolinea";
+            this.lblAerolinea.Size = new System.Drawing.Size(88, 13);
+            this.lblAerolinea.TabIndex = 11;
+            this.lblAerolinea.Text = "Aerolínea/Carrier";
+            // 
+            // txtDate
+            // 
+            this.txtDate.Enabled = false;
+            this.txtDate.Location = new System.Drawing.Point(168, 206);
+            this.txtDate.Name = "txtDate";
+            this.txtDate.Size = new System.Drawing.Size(102, 20);
+            this.txtDate.TabIndex = 12;
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(165, 229);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(65, 13);
+            this.lblDate.TabIndex = 13;
+            this.lblDate.Text = "Fecha/Date";
+            // 
+            // txtTime
+            // 
+            this.txtTime.Enabled = false;
+            this.txtTime.Location = new System.Drawing.Point(276, 206);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.Size = new System.Drawing.Size(71, 20);
+            this.txtTime.TabIndex = 14;
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(273, 229);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(58, 13);
+            this.lblTime.TabIndex = 15;
+            this.lblTime.Text = "Hora/Time";
+            // 
+            // txtAsiento
+            // 
+            this.txtAsiento.Enabled = false;
+            this.txtAsiento.Location = new System.Drawing.Point(353, 159);
+            this.txtAsiento.Name = "txtAsiento";
+            this.txtAsiento.Size = new System.Drawing.Size(98, 20);
+            this.txtAsiento.TabIndex = 16;
+            // 
+            // lblSeat
+            // 
+            this.lblSeat.AutoSize = true;
+            this.lblSeat.Location = new System.Drawing.Point(350, 182);
+            this.lblSeat.Name = "lblSeat";
+            this.lblSeat.Size = new System.Drawing.Size(69, 13);
+            this.lblSeat.TabIndex = 17;
+            this.lblSeat.Text = "Asiento/Seat";
+            // 
+            // txtPasaport
+            // 
+            this.txtPasaport.Enabled = false;
+            this.txtPasaport.Location = new System.Drawing.Point(353, 111);
+            this.txtPasaport.Name = "txtPasaport";
+            this.txtPasaport.Size = new System.Drawing.Size(98, 20);
+            this.txtPasaport.TabIndex = 18;
+            // 
+            // txtTill
+            // 
+            this.txtTill.Enabled = false;
+            this.txtTill.Location = new System.Drawing.Point(353, 206);
+            this.txtTill.Name = "txtTill";
+            this.txtTill.Size = new System.Drawing.Size(98, 20);
+            this.txtTill.TabIndex = 19;
+            // 
+            // lblPasaport
+            // 
+            this.lblPasaport.AutoSize = true;
+            this.lblPasaport.Location = new System.Drawing.Point(350, 134);
+            this.lblPasaport.Name = "lblPasaport";
+            this.lblPasaport.Size = new System.Drawing.Size(101, 13);
+            this.lblPasaport.TabIndex = 20;
+            this.lblPasaport.Text = "Pasaporte/Passport";
+            // 
+            // lblTill
+            // 
+            this.lblTill.AutoSize = true;
+            this.lblTill.Location = new System.Drawing.Point(350, 229);
+            this.lblTill.Name = "lblTill";
+            this.lblTill.Size = new System.Drawing.Size(51, 13);
+            this.lblTill.TabIndex = 21;
+            this.lblTill.Text = "Board Till";
+            // 
+            // rtbDatoTotal
+            // 
+            this.rtbDatoTotal.Enabled = false;
+            this.rtbDatoTotal.Location = new System.Drawing.Point(505, 111);
+            this.rtbDatoTotal.Name = "rtbDatoTotal";
+            this.rtbDatoTotal.Size = new System.Drawing.Size(240, 115);
+            this.rtbDatoTotal.TabIndex = 22;
+            this.rtbDatoTotal.Text = "";
+            // 
+            // FACTURA
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(716, 455);
-            this.Controls.Add(this.button1);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(781, 369);
+            this.Controls.Add(this.rtbDatoTotal);
+            this.Controls.Add(this.lblTill);
+            this.Controls.Add(this.lblPasaport);
+            this.Controls.Add(this.txtTill);
+            this.Controls.Add(this.txtPasaport);
+            this.Controls.Add(this.lblSeat);
+            this.Controls.Add(this.txtAsiento);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.txtTime);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.txtDate);
+            this.Controls.Add(this.lblAerolinea);
+            this.Controls.Add(this.txtAerolinea);
+            this.Controls.Add(this.lblTo);
+            this.Controls.Add(this.lblFrom);
+            this.Controls.Add(this.txtTo);
+            this.Controls.Add(this.txtFrom);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.btnFinalizar);
+            this.Controls.Add(this.btnNuevaCompra);
+            this.Controls.Add(this.btnRegresar3);
             this.Controls.Add(this.lblTitulo);
-            this.Name = "AEROLÍNEAS";
+            this.Name = "FACTURA";
             this.Text = "FACTURA";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -69,6 +293,27 @@
         #endregion
 
         private System.Windows.Forms.Label lblTitulo;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnRegresar3;
+        private System.Windows.Forms.Button btnNuevaCompra;
+        private System.Windows.Forms.Button btnFinalizar;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtFrom;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.Label lblFrom;
+        private System.Windows.Forms.Label lblTo;
+        private System.Windows.Forms.TextBox txtAerolinea;
+        private System.Windows.Forms.Label lblAerolinea;
+        private System.Windows.Forms.TextBox txtDate;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.TextBox txtAsiento;
+        private System.Windows.Forms.Label lblSeat;
+        private System.Windows.Forms.TextBox txtPasaport;
+        private System.Windows.Forms.TextBox txtTill;
+        private System.Windows.Forms.Label lblPasaport;
+        private System.Windows.Forms.Label lblTill;
+        private System.Windows.Forms.RichTextBox rtbDatoTotal;
     }
 }
