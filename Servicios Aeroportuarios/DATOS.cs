4408cbf09cc4d08cea6536fe791521191c2bd6d8
Theo-17 <mateo.espinoza@epn.edu.ec>
2022-02-21 01:00:13 -0500

    Cambios datos

--- Servicios Aeroportuarios/DATOS.cs
+++ Servicios Aeroportuarios/DATOS.cs
@@ -12,6 +12,34 @@ namespace Servicios_Aeroportuarios
 {
     public partial class Form1 : Form
     {
+
+        /*
+         * EJEMPLO DECLARACION DE ESTRUCTURA
+        public struct Aerolinea
+        {
+            public int asiento;
+            public String aerolinea;
+            public Destino destino;
+
+        }
+        static public Aerolinea Info;
+
+
+
+
+        public struct Persona
+        {
+            public String nombre;
+        }
+
+
+        static public Persona persona;
+
+
+
+        */
+
+
         public Form1()
         {
             InitializeComponent();
@@ -22,7 +50,7 @@ namespace Servicios_Aeroportuarios
             DESTINOS destino = new DESTINOS();
             this.Hide();
             destino.ShowDialog();
-            
+
         }
 
 
@@ -36,7 +64,9 @@ namespace Servicios_Aeroportuarios
 
         }
 
-
-
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            //txaDescripcion.AppendText("Apellidos: " + txtApellidos.Text);
+        }
     }
 }
